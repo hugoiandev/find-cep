1426fcc0d916922a61ce33d6c193a439a612a8e4
Hugo Ian <ahugaao@gmail.com>
2021-05-28 21:02:08 -0300

    Funcionalidade de buscar cep concluida

--- js/modulos/fetch-api-cep.js
+++ js/modulos/fetch-api-cep.js
@@ -1,14 +1,28 @@
 export default function initFetchApi(event) {
     event.preventDefault()
-    const cep = document.querySelector('#cep').value
-    const listCep = document.querySelectorAll('.list-cep')
-    console.log(listCep)
+    let cepDigitado = document.querySelector('#cep').value
+    const cep = document.querySelector('.cep')
+    const rua = document.querySelector('.logradouro')
+    const complemento = document.querySelector('.complemento')
+    const bairro = document.querySelector('.bairro')
+    const localidade = document.querySelector('.localidade')
+    const uf = document.querySelector('.uf')
+    const ddd = document.querySelector('.ddd')
 
     async function fetchApi() {
         try {
-            const apiCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            const apiCep = await fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`)
             const apiJson = await apiCep.json()
-            console.log(apiJson)
+
+            cep.innerHTML = apiJson.cep
+            rua.innerHTML = apiJson.logradouro
+            complemento.innerHTML = apiJson.complemento
+            bairro.innerHTML = apiJson.bairro
+            localidade.innerHTML = apiJson.localidade
+            uf.innerHTML = apiJson.uf
+            ddd.innerHTML = apiJson.ddd
+
+            cepDigitado = null
         }
         catch {
             console.log(Error('Ocorreu um erro!'))
